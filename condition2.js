6cb0e193f8aade82aefb524527b6054146b6e9bf
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-10-06 11:37:06 -0400

    can run multiple cnditions

---
+++ condition2.js
@@ -0,0 +1,2 @@
+console.log("Title 2");
+console.log("Content 2");
--- notifyme.cpp
+++ notifyme.cpp
@@ -4,62 +4,103 @@
 #include <array>
 #include <thread>
 #include <chrono>
+#include <fstream>
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #elif defined(__linux__)
 #include <unistd.h>
-#include <sys/wait.h>  
+#include <sys/wait.h>
+#include <vector>
+#include <sstream>
 #endif
 
-void sendNotification(const std::string &title, const std::string &content) {
+std::string runCommand(const std::string& command, const std::vector<std::string>& args) {
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        std::cerr << "Pipe failed!" << std::endl;
+        return "";
+    }
+
     pid_t pid = fork();
+    
+    if (pid < 0) {
+        std::cerr << "Fork failed!" << std::endl;
+        return "";
+    }
 
     if (pid == 0) {
-        const char *args[] = {"/usr/bin/notify-send", title.c_str(), content.c_str(), nullptr};
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
 
-        execv("/usr/bin/notify-send", const_cast<char *const *>(args));
-
-        std::cerr << "Error: execv failed to send notification!" << std::endl;
-        _exit(1); 
-    } else if (pid > 0) {
-        int status;
-        waitpid(pid, &status, 0); 
+        std::vector<char*> argv;
+        argv.push_back(const_cast<char*>(command.c_str()));
+        for (const auto& arg : args) {
+            argv.push_back(const_cast<char*>(arg.c_str()));
+        }
+        argv.push_back(nullptr);
+        execvp(argv[0], argv.data());
+        std::cerr << "execvp failed!" << std::endl;
+        exit(EXIT_FAILURE);
     } else {
-        std::cerr << "Error: Failed to fork a new process!" << std::endl;
+        close(pipefd[1]);
+        std::stringstream output;
+        char buffer[128];
+        ssize_t bytesRead;
+
+        while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0) {
+            buffer[bytesRead] = '\0';
+            output << buffer;
+        }
+        close(pipefd[0]);
+        int status;
+        waitpid(pid, &status, 0);
+        
+        if (!WIFEXITED(status)) {
+            std::cerr << "Child process did not exit normally!" << std::endl;
+        }
+
+        return output.str();
     }
 }
 
-std::pair<std::string, std::string> runJavaScriptAndCaptureOutput() {
-	std::array<char, 128> buffer;
-	std::string title, content;
+void sendNotification(const std::string &title, const std::string &content) {
+	runCommand("/usr/bin/notify-send", {title, content});
+}
 
-	FILE* pipe = popen("node condition.js", "r");
-	if (!pipe) {
-		std::cerr << "Error: Could not start JavaScript process!" << std::endl;
+std::pair<std::string, std::string> checkCondition(std::string condition_file) {
+	std::string output = runCommand("node", {condition_file});
+	if (output.empty()) {
 		return {"", ""};
 	}
 
-	if (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-		title = buffer.data();
-		title.erase(title.find_last_not_of("\n\r") + 1); 
-	}
-
-	if (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-		content = buffer.data();
-		content.erase(content.find_last_not_of("\n\r") + 1); 
-	}
-
-	pclose(pipe);
+	std::string title = output.substr(0, output.find('\n'));
+	std::string content = output.substr(output.find('\n') + 1);
 
 	return {title, content};
 }
 
 int main() {
-	while (true) {
-		auto [title, content] = runJavaScriptAndCaptureOutput();
+	std::ifstream file("conditions.txt");
+	if (!file.is_open()) {
+		std::cerr << "Error: Could not open conditions.txt!" << std::endl;
+		return 1;
+	}
 
-		if (!title.empty() && !content.empty()) {
-			sendNotification(title, content);
+	std::vector<std::string> condition_files;
+	std::string line;
+
+	while (file >> line) {
+		condition_files.push_back(line);
+	}
+	file.close();
+
+	while (true) {
+		for (const auto& condition_file : condition_files) {
+			auto [title, content] = checkCondition(condition_file);
+			if (!title.empty() && !content.empty()) {
+				sendNotification(title, content);
+			}
 		}
 
 		std::this_thread::sleep_for(std::chrono::seconds(60));
